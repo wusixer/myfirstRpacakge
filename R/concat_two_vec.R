4419d83e7abcee27567663e661b7d0306cf04621
Jiayi Cox <jiayisixer@gmail.com>
2021-05-10 20:39:41 -0400

    write docstring for func

--- R/concat_two_vec.R
+++ R/concat_two_vec.R
@@ -1,1 +1,12 @@
+#' concatenate two strings
+#'
+#' Create a new string from two existing strings by concatenating them
+#'
+#' @param vec1 string
+#' @param vec2 string
+#'
+#' @return string
+#' @export
+#' @examples
+#' concat_two_vec(c(1,2), c(3,4))
 concat_two_vec <- function(vec1, vec2){c(vec1,vec2)}
